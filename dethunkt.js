7fe5e8d22e757c7564479b9efe248d59206ed696
rektide <rektide@voodoowarez.com>
2014-10-02 22:49:17 -0400

    Stub out other surfaces of dbus bus.

--- dethunkt.js
+++ dethunkt.js
@@ -2,6 +2,16 @@ var dbus= require('dbus-native'),
   memoizee= require('memoizee'),
   genify= require('thunkify-wrap').genify
 
+function readFactory(val, def){
+	if(val && val.next){
+		return val.next()
+	}else if(val){
+		return val
+	}else{
+		return def.next()
+	}
+}
+
 function factoryGen(fn){
 	var _private;
 	function *_fnGen(){
@@ -12,43 +22,102 @@ function factoryGen(fn){
 	return _fnGen()
 }
 
-var sessionBus= factoryGen(function(){
+// buses
+var sessionBusFactory= factoryGen(function(){
 	return dbus.sessionBus()
 })
-
-var systemBus= factoryGen(function(){
+var systemBusFactory= factoryGen(function(){
 	return dbus.systemBus()
 })
 
+// services
 function *serviceGen(service, busFactory){
-	busFactory= busFactory|| sessionBus
 	var _service
 	while(1){
 		if(!_service)
-			_service= busFactory.next().getService(service)
+			_service= readFactory(busFactory, sessionBusFactory).getService(service)
 		yield service
 	}
 }
-
 var _services= memoizee(serviceGen)
 
-function service(service, bus){
-	_service(service, bus).next()
+function services(service, busFactory){
+	return _service(service, busFactory).next()
 }
 
-function getService(service, bus){
-	var interface= (function interface(object, interface){
-		var _service = services(service, bus)
-		var _interface= _service.getInferface(object, interface)
-		return genify(_interface)
-	})
-	return interface
+function getService(service, busFactory){
+	return {
+		interface: (function interface(object, interface){
+			var _service= services(service, readFactory(busFactory, sessionBusFactory))
+			var _interface= _service.getInferface(object, interface)
+			return genify(_interface)
+		}),
+		object: (function(objName){
+			var _service= services(service, readFactory(busFactory, sessionBusFactory))
+			var _object= _service.getObject(objName)
+			return genify(_object)
+		})
+	}
 }
 
 function getSystemService(service){
-	return getService(service, dbus.systemBus())
+	return getService(service, systemBusFactory)
 }
 
+// objects
+function *objectGen(path, name, busFactory){
+	var _object
+	while(1){
+		if(!_object)
+			_object= readFactory(busFactory, sessionBusFactory).getObject(path, name)
+		yield _object
+	}
+}
+var _objects= memoizee(objectGen)
+
+function getObject(path, name, busFactory){
+	return _objects(path, name, busFactory).next()
+}
+
+function getSystemObject(path, name){
+	return getObject(path, name, systemBusFactory)
+}
+
+// interfaces
+function *interfaceGen(path, objname, name, busFactory) {
+	var _interface
+	while(1){
+		if(!_interface)
+			_interface= readFactory(busFactory, sessionBusFactory).getInterface(path, objname, name)
+		yield _interface
+	}
+}
+var _interfaces= memoizee(interfaceGen)
+
+function getInterface(path, objname, name, busFactory){
+	return _interfaces(path, objname, name, busFactory).next()
+}
+
+function getSystemInterface(path, objname, name){
+	return getInterface(path, objname, name, systemBusFactory)
+}
+
+// default
 module.exports = getService
-module.exports.getSessionService = getService
-module.exports.getSystemService = getSystemService
+// default methods
+module.exports.getService = getService
+module.exports.getObject= getObject
+module.exports.getInterface= getInterface
+// named session
+module.exports.session= getService
+
+// top level system
+module.exports.getSystemService= getSystemService
+module.exports.getSystemObject= getSystemObject
+module.exports.getSystemInterface= getSystemInterface
+
+// named system
+module.exports.system= {}
+module.exports.system.getService= getSystemService
+module.exports.system.getObject= getSystemObject
+module.exports.system.getInterface= getSystemInterface
